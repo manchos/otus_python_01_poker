34e64a29914d7262dbd65ac499a0325c3f6a0fe5
manchos <vladm.ru@gmail.com>
2019-03-27 07:26:29 +0300

    add structure clasess

--- poker.py
+++ poker.py
@@ -1,3 +1,37 @@
+import collections
+from typing import NamedTuple
+
+
+# Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+
+class Card(NamedTuple):
+    rank: str
+    suit: str
+    def __str__(self):
+        return '{}{}'.format(self.rank, self.suit)
+
+
+
+class CardDeck:
+	ranks = [str(n) for n in range(2, 10)] + list('TJQKA')
+	suits = list('CSHD')
+
+	def __init__(self):
+		self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+
+class Hand:
+    def __init__(self, str_hand):
+        # if ()
+        # self._cards =
+        pass
+
+
+
 def hand_rank(hand):
     """Возвращает значение определяющее ранг 'руки'"""
     ranks = card_ranks(hand)
@@ -61,25 +95,25 @@ def best_wild_hand(hand):
 
 
 def test_best_hand():
-    print "test_best_hand..."
+    print("test_best_hand..."
     assert (sorted(best_hand("6C 7C 8C 9C TC 5C JS".split()))
             == ['6C', '7C', '8C', '9C', 'TC'])
     assert (sorted(best_hand("TD TC TH 7C 7D 8C 8S".split()))
             == ['8C', '8S', 'TC', 'TD', 'TH'])
     assert (sorted(best_hand("JD TC TH 7C 7D 7S 7H".split()))
             == ['7C', '7D', '7H', '7S', 'JD'])
-    print 'OK'
+    print('OK')
 
 
 def test_best_wild_hand():
-    print "test_best_wild_hand..."
+    print("test_best_wild_hand...")
     assert (sorted(best_wild_hand("6C 7C 8C 9C TC 5C ?B".split()))
             == ['7C', '8C', '9C', 'JC', 'TC'])
     assert (sorted(best_wild_hand("TD TC 5H 5C 7C ?R ?B".split()))
             == ['7C', 'TC', 'TD', 'TH', 'TS'])
     assert (sorted(best_wild_hand("JD TC TH 7C 7D 7S 7H".split()))
             == ['7C', '7D', '7H', '7S', 'JD'])
-    print 'OK'
+    print('OK')
 
 if __name__ == '__main__':
     test_best_hand()
